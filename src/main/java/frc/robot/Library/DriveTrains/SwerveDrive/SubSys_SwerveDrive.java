25af1f4a4e456a466ac16a912e119f558757a3fd
Rowan <rkortlandt@gmail.com>
2023-10-18 11:06:08 -0400

    start swerve stuff

--- src/main/java/frc/robot/Library/DriveTrains/SwerveDrive/SubSys_SwerveDrive.java
+++ src/main/java/frc/robot/Library/DriveTrains/SwerveDrive/SubSys_SwerveDrive.java
@@ -12,16 +12,47 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import frc.robot.Constants.Robot;
 import frc.robot.Library.DriveTrains.SwerveDrive.SwerveModules.MK4i_FalconFalcon.MK4i_FalconFalcon_Module;
 import frc.robot.Library.Gyroscopes.Pigeon2.SubSys_PigeonGyro;
+import java.io.File;
+import java.io.IOException;
+
+import swervelib.parser.SwerveControllerConfiguration;
+import swervelib.parser.SwerveDriveConfiguration;
+import swervelib.parser.SwerveParser;
+import swervelib.telemetry.SwerveDriveTelemetry;
+import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
+import swervelib.SwerveDrive;
 
 public class SubSys_SwerveDrive extends Subsystem {
   /** Creates a new SubSys_SwerveDrive. */
+  private SwerveDrive swerveDrive;
+  static File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(),"swerve");
+  
+  public SubSys_SwerveDrive () throws IOException {
+    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive();
+  }
 
-  /* ***** MK4i Swerve *****                                                         */
+  public SubSys_SwerveDrive (SwerveDriveConfiguration driveCfg, SwerveControllerConfiguration controllerCfg)
+  {
+    swerveDrive = new SwerveDrive(driveCfg, controllerCfg);
+  }
+
+  public void drive(Translation2d translation, double rotation, boolean fieldRelative)
+  {
+    swerveDrive.drive(
+      translation,
+      rotation,
+      fieldRelative,
+      false); // Open loop is disabled since it shouldn't be used most of the time.
+  }
+
+
   public MK4i_FalconFalcon_Module[] swerveModules;
 
   /* ***** SwrNStr_FalconPG71 Swerve *****                                           */
