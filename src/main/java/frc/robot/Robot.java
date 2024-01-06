77b8d2aba490832a2eb00118fc1fcffcf6c8417e
FunkyChicken <seanernstes@gmail.com>
2024-01-06 13:14:07 -0500

    Fix capital letter in package name

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,6 +13,8 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.library.pneumatics.SubSys_Pneumatics;
 
+import java.io.IOError;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -32,6 +34,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+
     CameraServer.startAutomaticCapture();
     robotContainer = new RobotContainer();
     m_subSys_Pneumatics.compressorOn();
