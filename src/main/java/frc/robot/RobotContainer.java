b7322187a6803461c36ff410caad67d26094ea91
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-01-20 16:32:02 -0500

    Tested on Robot.  Does not drive or steer properly yet.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,21 +7,25 @@
 
 package frc.robot;
 
+import java.io.File;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.library.vision.photonvision.SubSys_Photonvision;
-import frc.robot.chargedup.DriverStation;
 import frc.robot.chargedup.subsystems.arm.SubSys_Arm;
-import frc.robot.chargedup.subsystems.arm.commands.Cmd_SubSys_Arm_JoysticDefault;
-import frc.robot.chargedup.subsystems.arm.commands.Cmd_SubSys_Arm_RotateAndExtend;
 import frc.robot.chargedup.subsystems.bling.SubSys_Bling;
 import frc.robot.chargedup.subsystems.bling.SubSys_Bling_Constants;
 import frc.robot.chargedup.subsystems.bling.commands.Cmd_SubSys_Bling_SetColorValue;
 import frc.robot.chargedup.subsystems.hand.SubSys_Hand;
+import frc.robot.crescendo.DriverStation;
 import frc.robot.library.drivetrains.swerve_5152.SubSys_DriveTrain;
-import frc.robot.library.drivetrains.swerve_5152.commands.Cmd_SubSys_DriveTrain_JoysticDefault;
+import frc.robot.library.drivetrains.swerve_yagsl.SubSysSwerveYAGSL;
+import frc.robot.library.drivetrains.swerve_yagsl.commands.AbsoluteDriveAdv;
 import frc.robot.library.gyroscopes.pigeon2.SubSys_PigeonGyro;
 
 /**
@@ -38,49 +42,92 @@ public class RobotContainer {
   // ---- Power Distribution
   // private final PDPSubSys m_PDPSubSys = new PDPSubSys();
 
-  // ---- NavXGyro
-  // public final NavXGyroSubSys m_NavXGyroSubSys = new NavXGyroSubSys();
+  // ---- Drive Subsystem
+  // swerve_yagsl
+  private final SubSysSwerveYAGSL drivebase = new SubSysSwerveYAGSL(new File(Filesystem.getDeployDirectory(),
+                                                                         "swerve/mk4italonsl2"));
+  // swerve_5152
+  //public final SubSys_DriveTrain driveSubSys = new SubSys_DriveTrain(gyroSubSys);
+  
+  // swerve_5152 Pigeon2
+  //public final SubSys_PigeonGyro gyroSubSys = new SubSys_PigeonGyro();
 
-  // ---- Pigeon2
+  // Mecanum
+  // public final SubSys_MecanumDrive mecanumDriveSubSys = new SubSys_MecanumDrive();
 
-  public final SubSys_PigeonGyro gyroSubSys = new SubSys_PigeonGyro();
-
-
-  // ---- Drive Subsystem (Swerve)
-  public final SubSys_DriveTrain driveSubSys = new SubSys_DriveTrain(gyroSubSys);
-
-  public final SubSys_Photonvision photonvisionSubSys = new SubSys_Photonvision();
-
-  // private final PDPSubSys m_PDPSubSys = new PDPSubSys();
+  // ---- Photonvision
+  //public final SubSys_Photonvision photonvisionSubSys = new SubSys_Photonvision();
 
   // private final SubSys_LimeLight limeLightSubSys = new SubSys_LimeLight();
 
-  // public final SubSys_MecanumDrive mecanumDriveSubSys = new SubSys_MecanumDrive();
-
-  // public final SubSys_ColorSensor colorSubSys = new SubSys_ColorSensor();
-
-  // public final SubSys_DistanceSensor distanceSubsys = new SubSys_DistanceSensor();
   // ---- Driver Station
 
   // ---- Hand
-  public final SubSys_Hand handSubSys = new SubSys_Hand();
+  //public final SubSys_Hand handSubSys = new SubSys_Hand();
 
   // Arm
-  public final SubSys_Arm armSubSys = new SubSys_Arm(handSubSys.getHandLength());
+  //public final SubSys_Arm armSubSys = new SubSys_Arm(handSubSys.getHandLength());
 
-  public final SubSys_Bling blingSubSys = new SubSys_Bling();
-
-
+  //public final SubSys_Bling blingSubSys = new SubSys_Bling();
 
   public final DriverStation driverStationSubSys = new DriverStation();
-  public Auto auto;
+
+  //public Auto auto;
   public RobotContainer() {
-    auto = new Auto(blingSubSys, photonvisionSubSys, handSubSys, armSubSys, gyroSubSys, driveSubSys);
+    
     // Configure the button bindings
     configureButtonBindings();
 
-    // Configure default commands
+    /* 
+    AbsoluteDriveAdv closedAbsoluteDriveAdv = new AbsoluteDriveAdv(drivebase,
+                                                                   () -> MathUtil.applyDeadband(driverXbox.getLeftY(),
+                                                                                                OperatorConstants.LEFT_Y_DEADBAND),
+                                                                   () -> MathUtil.applyDeadband(driverXbox.getLeftX(),
+                                                                                                OperatorConstants.LEFT_X_DEADBAND),
+                                                                   () -> MathUtil.applyDeadband(driverXbox.getRightX(),
+                                                                                                OperatorConstants.RIGHT_X_DEADBAND),
+                                                                   driverXbox::getYButtonPressed,
+                                                                   driverXbox::getAButtonPressed,
+                                                                   driverXbox::getXButtonPressed,
+                                                                   driverXbox::getBButtonPressed);
+    */
+    
+    // Applies deadbands and inverts controls because joysticks
+    // are back-right positive while robot
+    // controls are front-left positive
+    // left stick controls translation
+    // right stick controls the desired angle NOT angular rotation
+    Command driveFieldOrientedDirectAngle = drivebase.driveCommand(
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveFwdAxis(), 0.1),
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveStrAxis(), 0.1),
+        () -> driverStationSubSys.DriveRotAxis(),
+        () -> driverStationSubSys.m_DriverController.getRightY());
 
+        
+
+    // Applies deadbands and inverts controls because joysticks
+    // are back-right positive while robot
+    // controls are front-left positive
+    // left stick controls translation
+    // right stick controls the angular velocity of the robot
+    Command driveFieldOrientedAnglularVelocity = drivebase.driveCommand(
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveFwdAxis(), 0.1),
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveStrAxis(), 0.1),
+        () -> driverStationSubSys.DriveRotAxis());
+
+    Command driveFieldOrientedDirectAngleSim = drivebase.simDriveCommand(
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveFwdAxis(), 0.1),
+        () -> MathUtil.applyDeadband(driverStationSubSys.DriveStrAxis(), 0.1),
+        () -> driverStationSubSys.DriveRotAxis());
+
+    
+        
+    //auto = new Auto(blingSubSys, photonvisionSubSys, handSubSys, armSubSys, gyroSubSys, driveSubSys);
+    
+    
+
+    // Configure default commands
+    drivebase.setDefaultCommand(driveFieldOrientedAnglularVelocity);
     /* Control System Components */
     /*
     armSubSys.setDefaultCommand(
@@ -118,15 +165,15 @@ public class RobotContainer {
   private void configureButtonBindings() {
 
     // Gyro Reset Command Button
-    driverStationSubSys.OpenHandButton.onTrue(new InstantCommand(handSubSys::OpenHand, handSubSys));
-    driverStationSubSys.CloseHandButton.onTrue(
-        new InstantCommand(handSubSys::CloseHand, handSubSys));
-    driverStationSubSys.GyroResetButton.onTrue(new InstantCommand(gyroSubSys::zeroYaw, gyroSubSys));
+    //driverStationSubSys.OpenHandButton.onTrue(new InstantCommand(handSubSys::OpenHand, handSubSys));
+    //driverStationSubSys.CloseHandButton.onTrue(
+    //    new InstantCommand(handSubSys::CloseHand, handSubSys));
+    //driverStationSubSys.GyroResetButton.onTrue(new InstantCommand(gyroSubSys::zeroYaw, gyroSubSys));
 
     // Gyro Reset Command Button
-    driverStationSubSys.PoseResetButton.onTrue(
+    //driverStationSubSys.PoseResetButton.onTrue(
         // new InstantCommand(driveSubSys::setPoseToOrigin, driveSubSys));
-        new InstantCommand(driveSubSys::setPoseToOrigin, driveSubSys));
+    //    new InstantCommand(driveSubSys::setPoseToOrigin, driveSubSys));
 
     /*    
     // Test Button
@@ -151,6 +198,7 @@ public class RobotContainer {
     */
 
     // CONE/CUBE SIGNALING
+    /* 
     driverStationSubSys.RequestConeButton.onTrue(
         new Cmd_SubSys_Bling_SetColorValue(
             blingSubSys, SubSys_Bling_Constants.Controllers.controller1, SubSys_Bling_Constants.SolidColors.Yellow));
@@ -174,6 +222,7 @@ public class RobotContainer {
             blingSubSys,
             SubSys_Bling_Constants.Controllers.controller1,
             SubSys_Bling_Constants.Patterns.FixedPalette.StrobeRed));
+    */
   }
 
   /**
@@ -182,6 +231,9 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    return auto.getAutoCommand();
+    //return auto.getAutoCommand();
+    return new Command() {
+        
+    };
   }
 }
