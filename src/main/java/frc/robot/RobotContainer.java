0ae9092c5d6c9f3bb1f5d6e5448500cb1c713f13
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-01 12:49:27 -0500

    Sean's Fixes while driving

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -54,9 +54,9 @@ public class RobotContainer {
 
   // ---- Drive Subsystem
   // swerve_ctre
-  private double MaxSpeed = 6; // 6 meters per second desired top speed
+  private double MaxSpeed = 1.5; // 6 meters per second desired top speed
   private double MaxAngularRate = 1.5 * Math.PI; // 3/4 of a rotation per second max angular velocity
-  private final CommandSwerveDrivetrain drivetrain = frc.robot.library.drivetrains.swerve_ctre.mk4il22023.TunerConstants_MK4iL2_2023.DriveTrain; // My drivetrain
+  private final CommandSwerveDrivetrain drivetrain = frc.robot.library.drivetrains.swerve_ctre.mk4il32024.TunerConstants_MK4iL3_2024.DriveTrain; // My drivetrain
 
 //  private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
   private final SwerveRequest.RobotCentric drive = new SwerveRequest.RobotCentric()
