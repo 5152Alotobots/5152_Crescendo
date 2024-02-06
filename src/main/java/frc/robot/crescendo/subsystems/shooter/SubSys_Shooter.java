a41e8191bee73271eea06911983d1b72db956267
FunkyChicken <seanernstes@gmail.com>
2024-02-05 19:18:28 -0500

    Add some basic arm shooter position control, .75 speed intake

--- src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter.java
+++ src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter.java
@@ -1,23 +1,18 @@
 package frc.robot.crescendo.subsystems.shooter;
+
 import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import com.ctre.phoenix6.configs.CANcoderConfiguration;
 import com.ctre.phoenix6.configs.CANcoderConfigurator;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.AbsoluteSensorRangeValue;
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-import com.ctre.phoenix6.signals.SensorDirectionValue;
-import com.revrobotics.CANSparkMax;
+import com.ctre.phoenix6.signals.*;
 import com.revrobotics.CANSparkLowLevel.MotorType;
-import edu.wpi.first.wpilibj.DigitalInput;
+import com.revrobotics.CANSparkMax;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.CAN_IDs;
-import frc.robot.crescendo.subsystems.intake.SubSys_Intake_Constants;
 
 /**
  * Handles outputs and inputs from the intake, including rotation motors and limit switches,
@@ -25,32 +20,35 @@ import frc.robot.crescendo.subsystems.intake.SubSys_Intake_Constants;
  */
 public class SubSys_Shooter extends SubsystemBase {
 
-    private final TalonSRX shooterWheelsMtr1 = new TalonSRX(CAN_IDs.ShooterWheelsMtr1_CAN_ID);
-    private final TalonSRX shooterWheelsMtr2 = new TalonSRX(CAN_IDs.ShooterWheelsMtr2_CAN_ID);
+    private final TalonSRX shooterWheelsMtr1 = new TalonSRX(CAN_IDs.ShooterWheelsMtrRight_CAN_ID);
+    private final TalonSRX shooterWheelsMtr2 = new TalonSRX(CAN_IDs.ShooterWheelsMtrLeft_CAN_ID);
     private final CANSparkMax shooterRollerMtr = new CANSparkMax(CAN_IDs.ShooterRollerMtr_CAN_ID, MotorType.kBrushless);
     // private final DigitalInput beamSensor = new DigitalInput(0);
     private final TalonFX shooterArmMtr = new TalonFX(CAN_IDs.ShooterArmMtr_CAN_ID);
-    private final TalonFXConfigurator shooterArmMtrConfigurator = shooterArmMtr.getConfigurator();
-    private final TalonFXConfiguration shooterArmMtrConfiguration = new TalonFXConfiguration();
     private final CANcoder shooterArmCANCoder = new CANcoder(CAN_IDs.ShooterArmCANCoder_CAN_ID);
-    private final CANcoderConfigurator shooterArmCANCoderConfigurator = shooterArmCANCoder.getConfigurator();
-    private final CANcoderConfiguration shooterArmCaNcoderConfiguration = new CANcoderConfiguration();
-    
     public SubSys_Shooter () {
 
         // Configure Shooter Arm Motor
+        TalonFXConfiguration shooterArmMtrConfiguration = new TalonFXConfiguration();
         shooterArmMtrConfiguration.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+        shooterArmMtrConfiguration.MotorOutput.Inverted = InvertedValue.Clockwise_Positive;
         shooterArmMtrConfiguration.Feedback.FeedbackRemoteSensorID = shooterArmCANCoder.getDeviceID();
         shooterArmMtrConfiguration.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.RemoteCANcoder;
+        shooterArmMtrConfiguration.Slot0.kP = 0.5;
+        shooterArmMtrConfiguration.Slot0.kI = 0;
+        shooterArmMtrConfiguration.Slot0.kD = 0;
         //shooterArmMtrConfiguration.Feedback.RotorToSensorRatio = 1;
-        
+
+        TalonFXConfigurator shooterArmMtrConfigurator = shooterArmMtr.getConfigurator();
         shooterArmMtrConfigurator.apply(shooterArmMtrConfiguration);
 
         // Configure Intake Arm CANcoder
+        CANcoderConfiguration shooterArmCaNcoderConfiguration = new CANcoderConfiguration();
         shooterArmCaNcoderConfiguration.MagnetSensor.AbsoluteSensorRange = AbsoluteSensorRangeValue.Signed_PlusMinusHalf;
-        shooterArmCaNcoderConfiguration.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+        shooterArmCaNcoderConfiguration.MagnetSensor.SensorDirection = SensorDirectionValue.Clockwise_Positive;
         shooterArmCaNcoderConfiguration.MagnetSensor.MagnetOffset = SubSys_Shooter_Constants.ShooterArm.CANcoderMagOffset;
 
+        CANcoderConfigurator shooterArmCANCoderConfigurator = shooterArmCANCoder.getConfigurator();
         shooterArmCANCoderConfigurator.apply(shooterArmCaNcoderConfiguration);
 
     }
@@ -75,4 +73,5 @@ public class SubSys_Shooter extends SubsystemBase {
     public void setShooterArmOutput(double percentOutput) {
         shooterArmMtr.set(percentOutput);
     }
+
 }
