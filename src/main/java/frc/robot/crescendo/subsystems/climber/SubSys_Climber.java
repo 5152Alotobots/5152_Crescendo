3f9f5c4d4ce16c4572f92886f7bc4fc18771d739
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-10 21:10:17 -0500

    Started Simulation

--- src/main/java/frc/robot/crescendo/subsystems/climber/SubSys_Climber.java
+++ src/main/java/frc/robot/crescendo/subsystems/climber/SubSys_Climber.java
@@ -65,4 +65,9 @@ final PositionVoltage climberRightPid;
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+   @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run
+  }
 }
