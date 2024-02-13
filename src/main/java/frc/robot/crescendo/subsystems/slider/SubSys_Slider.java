84d20a8c57e3115976e24b669b3b44969bc9f9b8
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-12 19:34:44 -0500

    Added Solenoids to Slider SubSys

--- src/main/java/frc/robot/crescendo/subsystems/slider/SubSys_Slider.java
+++ src/main/java/frc/robot/crescendo/subsystems/slider/SubSys_Slider.java
@@ -4,14 +4,36 @@
 
 package frc.robot.crescendo.subsystems.slider;
 
+import edu.wpi.first.wpilibj.Compressor;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class SubSys_Slider extends SubsystemBase {
   /** Creates a new SubSys_Slider. */
+
+  private final DoubleSolenoid slider_leftDoubleSolenoid =
+      new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
+        
+  private final DoubleSolenoid slider_rightDoubleSolenoid =
+      new DoubleSolenoid(PneumaticsModuleType.REVPH, 1, 2);
+       
+  private final Compressor m_compressor = new Compressor(PneumaticsModuleType.REVPH);
+
   public SubSys_Slider() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  public void sliderExtendCmd() {
+    slider_leftDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+    slider_rightDoubleSolenoid.set(DoubleSolenoid.Value.kForward);
+  }
+
+  public void sliderRetractCmd() {
+    slider_leftDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
+    slider_rightDoubleSolenoid.set(DoubleSolenoid.Value.kReverse);
   }
 }
