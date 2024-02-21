8decbf396e3c4fa1de8efc9e007a997bdcfd7406
FunkyChicken <seanernstes@gmail.com>
2024-02-21 12:08:14 -0500

    Added shooter math to a command, angle of shooter based on pose still has to be figured out

--- src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter_Constants.java
+++ src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter_Constants.java
@@ -12,11 +12,17 @@ public class SubSys_Shooter_Constants {
     public static final class MaxSpeeds {
         public static final double MAX_ARM_ROTATION_SPEED = 0.15;
         public static final double MAX_SHOOTER_SPEED = 1;
+        public static final double MAX_SHOOTER_SPPED_MPS = 6.2;
         public static final double MAX_INTAKE_SPEED = 1;
         public static final double SLOW_INTAKE_SPEED = 0.4;
     }
 
+    public static final class AutoAim {
+        public static final double LAUNCH_TOLERANCE = 1; // +- 1 Meter to launch
+    }
+
     public static final class FieldConstants {
         public static final Pose2d SPEAKER_XY = new Pose2d(15.55, 5.45, new Rotation2d(0));
+
     }
 }
