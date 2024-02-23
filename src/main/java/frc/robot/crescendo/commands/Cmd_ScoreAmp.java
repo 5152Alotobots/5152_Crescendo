e58046d7a06d2b097eebf9c61dffa8c11d90211e
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-22 22:13:12 -0500

    Added New Commands to Create

--- src/main/java/frc/robot/crescendo/commands/Cmd_ScoreAmp.java
+++ src/main/java/frc/robot/crescendo/commands/Cmd_ScoreAmp.java
@@ -4,9 +4,12 @@
 
 package frc.robot.crescendo.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 
-public class Cmd_ScoreAmp extends Command {
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class Cmd_ScoreAmp extends SequentialCommandGroup {
   /** Creates a new Cmd_ShootAmp. 
    * This Command will start from the defined pose in front of the Amp, aim drive forward, score the note and back off.
    * 1. Aim the Shooter for the Amp
@@ -16,24 +19,8 @@ public class Cmd_ScoreAmp extends Command {
    * 5. Stow the Shooter
   */
   public Cmd_ScoreAmp() {
-    // Use addRequirements() here to declare subsystem dependencies.
-  }
-
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {}
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {}
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {}
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+    addCommands();
   }
 }
--- src/main/java/frc/robot/crescendo/commands/Cmd_TransferIntake2Shooter.java
+++ src/main/java/frc/robot/crescendo/commands/Cmd_TransferIntake2Shooter.java
@@ -9,16 +9,17 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class Cmd_ScoreSpeakerRight extends SequentialCommandGroup {
-  /** Creates a new Cmd_Shoot Speaker Right. 
+public class Cmd_TransferIntake2Shooter extends SequentialCommandGroup {
+  /** Creates Command to Transfer Note from the Shooter to the Shooter 
    * This Command will start from the defined pose in front of the Amp, aim drive forward, score the note and back off.
-   * 1. Aim the Shooter for the Speaker
+   * 1. Aim the Shooter for the Source
    * 2. Drive forward
-   * 3. Shoot the Note
+   * 3. Shooter Intake the note
+   * 4. Shooter Roller position the note
    * 4. Drive backward
    * 5. Stow the Shooter
   */
-  public Cmd_ScoreSpeakerRight() {
+  public Cmd_TransferIntake2Shooter() {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands();
