38d05494042f38f1c16d6c01ba221f3ae34a8b1d
chrisdeboer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-24 17:40:18 -0500

    Created Triggers for all Trigger Axis

--- src/main/java/frc/robot/crescendo/HMIStation.java
+++ src/main/java/frc/robot/crescendo/HMIStation.java
@@ -44,52 +44,30 @@ public class HMIStation {
   public final POVButton shooterSlowButton = new POVButton(driverController, 270);
 
   // Driver Axes
-  /**
-   * Gets the forward axis value for driving.
-   * 
-   * @return The value used for driving forward. unmodified. 
-   */
-  public double driveFwdAxisRaw() {
+   public double driveFwdAxisRaw() {
     //return -1*driverController.getRawAxis(1);
     return driveXSpdFilter.calculate(-1*driverController.getRawAxis(1));
   }
 
-  /**
-   * Gets the strafe axis value for driving. unmodified.
-   *
-   * @return The strafe axis value.
-   */ 
   public double driveStrAxisRaw() {
     //return -1*driverController.getRawAxis(0);
     return driveYSpdFilter.calculate(-1*driverController.getRawAxis(0));
   }
 
-  /**
-   * Gets the rotation axis value for driving. unmodified.
-   * @return The rotation axis value.
-   */ 
-  public double driveRotAxisRaw() {
+   public double driveRotAxisRaw() {
     return -1*driverController.getRawAxis(4);
   }
   
   // Driver Trigger Axes
-  /**
-   * Checks if the performance mode A is active.
-   *
-   * @return True if the mode is active, false otherwise.
-   */
-  public boolean shooterIn() {
+  public boolean shooterInTriggerAxis() {
     return (driverController.getRawAxis(2) > 0.3);
   }
+  public final Trigger shooterIn = new Trigger(() -> shooterInTriggerAxis());
 
-  /**
-   * Checks if the performance mode B is active.
-   *
-   * @return True if the mode is active, false otherwise.
-   */
-  public boolean shooterOut() {
+  public boolean shooterOutTriggerAxis() {
     return (driverController.getRawAxis(3) > 0.3);
   }
+  public final Trigger shooterOut = new Trigger(() -> shooterOutTriggerAxis());
   
   // **** Co-Driver Controller ****
   private final XboxController coDriverController = new XboxController(1);
@@ -98,8 +76,6 @@ public class HMIStation {
   public final JoystickButton shooterRollerOutSlow = new JoystickButton(coDriverController, 1);
   public final JoystickButton shooterSpeakerPos = new JoystickButton(coDriverController, 2);
   //public final JoystickButton shooterAmpPos = new JoystickButton(coDriverController, 3);
-  public final Trigger shooterShoot = new Trigger(() -> shooterOut());
-  public final Trigger shooterIn = new Trigger(() -> shooterIn());
   public final JoystickButton shooterRollerInSlow = new JoystickButton(coDriverController, 4);
   public final JoystickButton intakeOut = new JoystickButton(coDriverController, 5);
   public final JoystickButton intakeIn = new JoystickButton(coDriverController, 6);
@@ -111,78 +87,53 @@ public class HMIStation {
   //public final POVButton blankCoDriverPOVLeftButton = new POVButton(coDriverController, 270);
 
   // Co-Driver Axes
-  /**
-   * Gets the forward axis value for driving.
-   * 
-   * @return The value used for driving forward. unmodified. 
-   */
   public double shooterArmAxisRaw() {
     return -1*coDriverController.getRawAxis(5);
   }
-  /**
-   * Gets the rotation axis value for driving. unmodified.
-   *
-   * @return The rotation axis value.
-   */ 
+
   public double intakeArmAxisRaw() {
     return coDriverController.getRawAxis(1);
   }
-  // Driver Trigger Axes
-  /**
-   * Checks if the performance mode A is active.
-   * @return True if the mode is active, false otherwise.
-   */
-  public boolean shooterInCoDrvr() {
+
+  public boolean shooterInCoDrvrTriggerAxis() {
     return (coDriverController.getRawAxis(3) > 0.3);
   }
+  public final Trigger shooterInCoDrvr = new Trigger(() -> shooterInCoDrvrTriggerAxis());
 
-  /**
-   * Checks if the performance mode B is active.
-   * @return True if the mode is active, false otherwise.
-   */
-  public boolean shooterSourcePos() {
+  public boolean shooterArmSourcePosTriggerAxis() {
     return (driverController.getRawAxis(2) > 0.3);
   }
+  public final Trigger shooterArmSourcePos = new Trigger(() -> shooterArmSourcePosTriggerAxis());
   
   // Aux Driver Controller
   //private final XboxController auxdriverController = new XboxController(2);
 
   // Button Box
 
-  /**
-     * 
-     * @return
-     */
-    public double getDriveXYPerfMode(){
-        double xySpeed = Calibrations.DriveTrain.PerformanceMode_Default.DriveTrainMaxSpd;
-        if(turtleModeButton.getAsBoolean()){
-            xySpeed = Calibrations.DriveTrain.PerformanceMode_Turtle.DriveTrainMaxSpd;
-        }else if(turboModeButton.getAsBoolean()){
-            xySpeed = Calibrations.DriveTrain.PerformanceMode_Turbo.DriveTrainMaxSpd;
-        }
-        return driveSpdPerfModeSwFilter.calculate(xySpeed);
+ 
+  public double getDriveXYPerfMode(){
+    double xySpeed = Calibrations.DriveTrain.PerformanceMode_Default.DriveTrainMaxSpd;
+    if(turtleModeButton.getAsBoolean()){
+      xySpeed = Calibrations.DriveTrain.PerformanceMode_Turtle.DriveTrainMaxSpd;
+    }else if(turboModeButton.getAsBoolean()){
+      xySpeed = Calibrations.DriveTrain.PerformanceMode_Turbo.DriveTrainMaxSpd;
     }
+      return driveSpdPerfModeSwFilter.calculate(xySpeed);
+  }
 
-     /**
-     * 
-     * @return
-     */
-    public double getDriveRotPerfMode(){
-        double rotSpeed = Calibrations.DriveTrain.PerformanceMode_Default.DriveTrainMaxRotSpd;
-        if(turtleModeButton.getAsBoolean()){
-            rotSpeed = Calibrations.DriveTrain.PerformanceMode_Turtle.DriveTrainMaxRotSpd;
-        }else if(turboModeButton.getAsBoolean()){
-            rotSpeed = Calibrations.DriveTrain.PerformanceMode_Turbo.DriveTrainMaxRotSpd;
-        }
-        return driveRotPerfModeSwFilter.calculate(rotSpeed);
+  public double getDriveRotPerfMode(){
+    double rotSpeed = Calibrations.DriveTrain.PerformanceMode_Default.DriveTrainMaxRotSpd;
+    if(turtleModeButton.getAsBoolean()){
+      rotSpeed = Calibrations.DriveTrain.PerformanceMode_Turtle.DriveTrainMaxRotSpd;
+    }else if(turboModeButton.getAsBoolean()){
+      rotSpeed = Calibrations.DriveTrain.PerformanceMode_Turbo.DriveTrainMaxRotSpd;
     }
+      return driveRotPerfModeSwFilter.calculate(rotSpeed);
+  }
   
   // ---- Alliance Color
   public void alliancePosition(){
     DriverStation.getAlliance();
   }
-
-
-
 
 }
