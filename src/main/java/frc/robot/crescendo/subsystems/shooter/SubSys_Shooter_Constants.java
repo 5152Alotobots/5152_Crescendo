3224a0c5a6c3526ab55af5c8f4e625584fc124d6
Chris DeBoer <45087060+chrisdeboer@users.noreply.github.com>
2024-02-28 20:55:01 -0500

    Fixed Encoders

--- src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter_Constants.java
+++ src/main/java/frc/robot/crescendo/subsystems/shooter/SubSys_Shooter_Constants.java
@@ -6,7 +6,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 public class SubSys_Shooter_Constants {
     
     public static final class ShooterArm {
-        public static final double CANcoderMagOffset = 0.0; // -.89;
+        public static final double CANcoderMagOffset = -0.9; // -.89;
 
         public static final double ShooterArmTransferPos = 45;  // Transfer Position Degrees
         public static final double ShooterArmAmpPos = 45;       // Amp Scoring Position Degrees
