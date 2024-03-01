0842bbb4bbe75e240976b8abf8012f223c8559c2
FunkyChicken <seanernstes@gmail.com>
2024-02-29 23:21:49 -0500

    Remove auto camera capture and deprecate Auto.java for now to save scheduler runtime

--- src/main/java/frc/robot/Auto.java
+++ src/main/java/frc/robot/Auto.java
@@ -1,20 +1,21 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.library.vision.photonvision.SubSys_Photonvision;
 import frc.robot.chargedup.subsystems.arm.SubSys_Arm;
 import frc.robot.chargedup.subsystems.bling.SubSys_Bling;
 import frc.robot.chargedup.subsystems.hand.SubSys_Hand;
 import frc.robot.library.drivetrains.swerve_5152.SubSys_DriveTrain;
-import frc.robot.library.gyroscopes.pigeon2.SubSys_PigeonGyro; 
+import frc.robot.library.gyroscopes.pigeon2.SubSys_PigeonGyro;
+import frc.robot.library.vision.photonvision.SubSys_Photonvision;
 
 /**
  * The Auto class is responsible for handling the autonomous mode of the robot. It includes a
  * variety of commands for different tasks such as escaping, state middle leave, state barrier, and
  * others. The class also includes a SendableChooser object to select the desired command.
+ * @deprecated Use PathPlanner Inline RobotContainer commands/autos instead
  */
+@Deprecated
 public class Auto {
   private final SendableChooser<Command> m_chooser = new SendableChooser<>();
 
