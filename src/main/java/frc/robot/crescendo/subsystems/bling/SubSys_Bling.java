a6cefa2c63105a2477c0bb04646ffe4f8828a685
FunkyChicken <seanernstes@gmail.com>
2024-03-06 10:27:48 -0500

    Bling WIP

---
+++ src/main/java/frc/robot/crescendo/subsystems/bling/SubSys_Bling.java
@@ -0,0 +1,30 @@
+package frc.robot.crescendo.subsystems.bling;
+
+import com.ctre.phoenix.led.CANdle;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import static frc.robot.Constants.CAN_IDs.CANDLE_CAN_ID;
+import static frc.robot.crescendo.subsystems.bling.SubSys_Bling_Constants.LED_TYPE;
+import static frc.robot.crescendo.subsystems.bling.SubSys_Bling_Constants.MAX_LED_BRIGHTNESS;
+
+public class SubSys_Bling extends SubsystemBase {
+    private final CANdle controller = new CANdle(CANDLE_CAN_ID);
+
+    public SubSys_Bling() {
+        controller.configBrightnessScalar(MAX_LED_BRIGHTNESS);
+        controller.configLEDType(LED_TYPE);
+        // Alliance colors
+        if (DriverStation.getAlliance().isPresent()) {
+            if (DriverStation.getAlliance().get().equals(DriverStation.Alliance.Blue)) {
+                controller.setLEDs(0, 0, 255);
+            }
+        } else {
+            controller.setLEDs(255, 0, 0);
+        }
+    }
+
+    @Override
+    public void periodic() {
+    }
+}
---
+++ src/main/java/frc/robot/crescendo/subsystems/bling/SubSys_Bling_Constants.java
@@ -0,0 +1,17 @@
+package frc.robot.crescendo.subsystems.bling;
+
+import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.ColorFlowAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
+
+public class SubSys_Bling_Constants {
+    public static final double MAX_LED_BRIGHTNESS = 1;
+    public static final int NUM_LEDS = 50;
+    public static final CANdle.LEDStripType LED_TYPE = CANdle.LEDStripType.BRG;
+
+    public static final class Animations {
+        public static final ColorFlowAnimation transferAnimation = new ColorFlowAnimation(255, 145, 0, 0, 1, NUM_LEDS, ColorFlowAnimation.Direction.Forward);
+        public static final StrobeAnimation noteInIntake = new StrobeAnimation(0, 255, 174, 0, 0.4, NUM_LEDS);
+        public static final StrobeAnimation noteInShooter = new StrobeAnimation(0, 179, 255, 0, 0.4, NUM_LEDS);
+    }
+}
